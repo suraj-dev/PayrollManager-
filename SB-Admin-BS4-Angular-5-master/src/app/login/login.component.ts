5c4560602db1b9c8b9c9c14d7d0823d7d4ee1cd5
suraj-dev <suraj.scorp11@gmail.com>
2018-01-06 03:45:32 -0500

    added placeholder data

--- SB-Admin-BS4-Angular-5-master/src/app/login/login.component.ts
+++ SB-Admin-BS4-Angular-5-master/src/app/login/login.component.ts
@@ -9,17 +9,12 @@ import { routerTransition } from '../router.animations';
     animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
-    @Output() isLoggedIn : EventEmitter<boolean>;
     constructor(public router: Router) {
-        this.isLoggedIn = new EventEmitter<boolean>();
     }
 
     ngOnInit() {}
 
     onLoggedin() {
-       // localStorage.setItem('isLoggedin', 'true');
-        this.isLoggedIn.emit(true);
-        localStorage["loggedIn"] = true;
-        console.log("On Login Component - " + localStorage["loggedIn"]);
+        localStorage.setItem('isLoggedin', 'true');
     }
 }
